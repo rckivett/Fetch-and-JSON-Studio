4ff940b8ddb72a792fa197e5c4a456def55599fc
rckivett <rckivett@gmail.com>
2022-08-11 20:25:59 -0500

    studio solution

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // TODO: add code here
 window.addEventListener("load", function(){
-  fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
-console.log(response);
-});
-  
+  this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+    return response.json();
+  }).then(function(json){
+    console.log(json);
+    const container = document.querySelector('#container');
+    let astronauts = '';
+    for (astronaut of json){
+      astronauts +=`
+    <div class="astronaut">
+    <div class="bio">
+    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+    <ul>
+    <li>Hours in space: ${astronaut.hoursInSpace}</li>
+    <li>Active: ${astronaut.active}</li>
+    <li>Skills: ${astronaut.skills.toString()}</li>
+    </ul>
+    </div>
+    <img class = "avatar" src = "${astronaut.picture}">
+    </div>
+    `;
+    }
+  container.innerHTML = astronauts;
+  });
 });
