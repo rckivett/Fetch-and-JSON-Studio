44586b2e40686515eec351a55a1e8e89397a1d9d
rckivett <rckivett@gmail.com>
2022-08-11 21:02:46 -0500

    added bonus mission text in script.js and style.css

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ window.addEventListener("load", function(){
     console.log(json);
     const container = document.querySelector('#container');
     let astronauts = '';
-    for (astronaut of json){
+    for (astronaut of json.sort((a, b) => b.hoursInSpace - a.hoursInSpace)){
       astronauts +=`
     <div class="astronaut">
     <div class="bio">
     <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
     <ul>
     <li>Hours in space: ${astronaut.hoursInSpace}</li>
-    <li>Active: ${astronaut.active}</li>
-    <li>Skills: ${astronaut.skills.toString()}</li>
+    <li>Active: <span class="${astronaut.active ? "active" : "inactive"}">${astronaut.active}</span></li>
+    <li>Skills: ${astronaut.skills.join(", ")}</li>
     </ul>
     </div>
     <img class = "avatar" src = "${astronaut.picture}">
